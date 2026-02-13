b2ffca3e4ec7d587e0178660e28bc2d45bea2069
BikramChatterjee9 <34390859+BikramChatterjee9@users.noreply.github.com>
2026-02-14 00:01:14 +0530

    changes

---
+++ tests/test1.spec.ts
@@ -0,0 +1,19 @@
+import {test,expect} from '@playwright/test'
+
+test('verify 1',async({page})=>{
+
+    await page.goto('https://testautomationpractice.blogspot.com/')
+    console.log('test')
+})
+
+test('verify 2',async({page})=>{
+
+    await page.goto('https://testautomationpractice.blogspot.com/')
+    console.log('test')
+})
+
+test('verify 3',async({page})=>{
+
+    await page.goto('https://testautomationpractice.blogspot.com/')
+    console.log('test')
+})
