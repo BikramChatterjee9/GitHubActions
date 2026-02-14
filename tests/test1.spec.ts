cd23a26603ccdecf0f34f2b249dcdba29eec8d2b
BikramChatterjee9 <34390859+BikramChatterjee9@users.noreply.github.com>
2026-02-14 22:40:34 +0530

    test

--- tests/test1.spec.ts
+++ tests/test1.spec.ts
@@ -4,16 +4,19 @@ test('verify 1',async({page})=>{
 
     await page.goto('https://testautomationpractice.blogspot.com/')
     console.log('test')
+    expect(true).toBe(true)
 })
 
 test('verify 2',async({page})=>{
 
     await page.goto('https://testautomationpractice.blogspot.com/')
     console.log('test')
+    expect(true).toBe(true)
 })
 
 test('verify 3',async({page})=>{
 
     await page.goto('https://testautomationpractice.blogspot.com/')
     console.log('test')
+    expect(true).toBe(false)
 })
