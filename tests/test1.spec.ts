4a6eb3eb9164a945d52587cc459501135cc57f15
BikramChatterjee9 <34390859+BikramChatterjee9@users.noreply.github.com>
2026-02-14 22:19:14 +0530

    test

--- tests/test1.spec.ts
+++ tests/test1.spec.ts
@@ -3,17 +3,17 @@ import {test,expect} from '@playwright/test'
 test('verify 1',async({page})=>{
 
     await page.goto('https://testautomationpractice.blogspot.com/')
-    console.log('test')
+    console.log('test 1')
 })
 
 test('verify 2',async({page})=>{
 
     await page.goto('https://testautomationpractice.blogspot.com/')
-    console.log('test')
+    console.log('test 2')
 })
 
 test('verify 3',async({page})=>{
 
     await page.goto('https://testautomationpractice.blogspot.com/')
-    console.log('test')
+    console.log('test 3')
 })
